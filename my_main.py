c74c2ffbf9f0c3c5f18c95a8a875d58fbe1e7060
Jamal Taha <jamaltaha197@gmail.com>
2026-03-25 21:10:34 +0100

    Add files via upload

---
+++ my_main.py
@@ -0,0 +1,9 @@
+score = float(input("Enter your test score: "))
+if score >= 90:
+    print("You got an A grade!")
+elif score >= 75:
+    print("You got a B grade!")
+elif score >= 50:
+    print("You got C grade!")
+else:
+    print("You got F grade.")
